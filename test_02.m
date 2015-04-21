74ac86d552a9ecbec064762fafb443c1abf36a0b
IngJohEricsson <ingemar.s.johansson@ericsson.com>
2015-04-21 14:25:02 +0200

    Modified m files
    Signed-off-by: IngJohEricsson <ingemar.s.johansson@ericsson.com>

--- test_02.m
+++ test_02.m
@@ -4,27 +4,38 @@ function plot_all(a,Tmax,Bmax,Cmax)
     %Tmax = 100.0;
     figure(1);
     subplot(2,1,1);
-    plot(T,a(:,2),T,a(:,4));
+    plot(T,a(:,2));%,T,a(:,4));
+        set(gca,'FontSize',12);grid on;
+
     axis([0 Tmax 0 0.2]);grid on;
-    title('OWD and OWD target [s]');
-    legend('OWD','OWD target');
+    title('OWD [s]');
+    %legend('OWD','OWD target');
     subplot(2,1,2);
-    plot(T,a(:,15),T,a(:,19),'r');
+    plot(T,a(:,15),T,a(:,22),'r');
+        set(gca,'FontSize',12);grid on;
+
     title('RTP queue delay [s]');    
-    legend('Video','Audio');
-    axis([0 Tmax 0 0.03]);grid on;
+    legend('Stream 1','Stream 2');
+    axis([0 Tmax 0 0.1]);grid on;
     xlabel('T [s]');
     figure(2);
     subplot(2,1,1);
-    plot(T,a(:,8),T,a(:,9),T,a(:,10),T,a(:,11),T,a(:,K)*1000);
+    plot(T,a(:,8),T,a(:,K)*10000-5000,'r.');
+        set(gca,'FontSize',12);grid on;
+
     title('CWND [byte]');    
-    legend('CWND','CWND infl. point','Max bytes in flight','Bytes in flight','In fast start');
+    legend('CWND','In fast start');
     axis([0 Tmax 0 Cmax]);grid on;    
     subplot(2,1,2);
-    plot(T,a(:,17),T,a(:,K)*1e5,T,a(:,16),T,a(:,20));
+    K=100;B_ = ones(1,K)/K;
+    TTT = [00 39.9 40 59.9 60 100];
+    BBB = [5e6 5e6 1e6 1e6 5e6 5e6];
+    plot(T,a(:,16),T,a(:,23),T,filter(B_,1,a(:,20)+a(:,27)),'k',TTT,BBB,'r');
+        set(gca,'FontSize',12);grid on;
+
     axis([0 Tmax 0 Bmax]);grid on;
     title('Bitrate [bps]');    
-    legend('Max bitrate','In fast start','Target bitrate video','Target bitrate audio');
+    legend('Target, stream 1','Target, stream 2','Throughput');
     xlabel('T [s]');
 
     
