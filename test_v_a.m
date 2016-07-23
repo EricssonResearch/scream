06c5c770a8e5c7d28c6eb6ec83d03e9fa92c299b
IngJohEricsson <ingemar.s.johansson@ericsson.com>
2016-07-23 20:46:38 +0200

    Minor mod
    Signed-off-by: IngJohEricsson <ingemar.s.johansson@ericsson.com>

--- test_v_a.m
+++ test_v_a.m
@@ -9,7 +9,7 @@ function test_v_a(a,Tmax,I,Bmax,Cmax)
     plot(T,a(:,2),T,a(:,3)); 
     set(gca,'FontSize',12);grid on;
     set(gca,'XTickLabel',[]);    
-    axis([0 Tmax 0 0.5]);grid on;
+    axis([0 Tmax 0 0.3]);grid on;
     title('qdel[s] and qdel trend');
     %legend('OWD','OWD trend');
     subplot(2,1,2);
@@ -17,12 +17,13 @@ function test_v_a(a,Tmax,I,Bmax,Cmax)
     set(gca,'FontSize',12);grid on;
     axis([0 Tmax 0 Cmax]);grid on;    
     title('CWND & in flight [byte]');    
+    xlabel('T [s]');
 
     figure(2);
     subplot(2,1,1);
     plot(T,a(:,14)); 
     set(gca,'FontSize',12);grid on;
-    axis([0 Tmax 0 0.2]);grid on;
+    axis([0 Tmax 0 0.1]);grid on;
     set(gca,'XTickLabel',[]);
     title('RTP queue delay');
     subplot(2,1,2);
@@ -38,7 +39,7 @@ function test_v_a(a,Tmax,I,Bmax,Cmax)
     subplot(2,1,1);
     plot(T,a(:,14+7)); 
     set(gca,'FontSize',12);grid on;
-    axis([0 Tmax 0 0.2]);grid on;
+    axis([0 Tmax 0 0.1]);grid on;
     set(gca,'XTickLabel',[]);
     title('RTP queue delay');
     subplot(2,1,2);
