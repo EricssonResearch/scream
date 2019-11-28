e500256567af27274a2a31b484a3279ae71287b1
Ingemar Johansson <ingemar.s.johansson@ericsson.com>
2019-11-28 14:30:21 +0100

    Added project files

--- code/scream_v_a.cpp
+++ code/scream_v_a.cpp
@@ -13,12 +13,12 @@ using namespace std;
 const float Tmax = 100;
 const bool isChRate = true;
 const bool printLog = true;
-const bool ecnCapable = true;
-const bool isL4s = true;
+const bool ecnCapable = false;
+const bool isL4s = false;
 const float FR = 25.0f;
 int swprio = -1;
-#define TRACEFILE "../traces/trace_no_key_smooth.txt"
-//#define TRACEFILE "../traces/trace_key.txt"
+//#define TRACEFILE "../traces/trace_no_key_smooth.txt"
+#define TRACEFILE "../traces/trace_key.txt"
 //#define TRACEFILE "../traces/trace_no_key.txt"
 /*
 * Mode determines how many streams should be run
