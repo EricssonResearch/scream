9bf048ecf6dea0ea8ec88044025f1f10c79d9659
mirabilos <t.glaser@tarent.de>
2022-06-27 19:45:49 +0200

    fix whitespace: files with trailing CR

--- multicam/sender/scream/code/scream_sender.cpp
+++ multicam/sender/scream/code/scream_sender.cpp
@@ -22,7 +22,7 @@ struct sigaction sa;
 using namespace std;
 
 /*
- * Stub function 
+ * Stub function
  */
 void packet_free(void* a) {//
 }
@@ -164,7 +164,7 @@ void readList(char *buf, float *list) {
 	   	s = strtok(NULL,":");
    }
 }
-	
+
 int main(int argc, char* argv[]) {
 
    struct timeval tp;
@@ -200,7 +200,7 @@ int main(int argc, char* argv[]) {
         cerr << "    example -priority 1.0:0.5:0.2:0.1" << endl;
         cerr << " -ratescale list    : Compensate for systematic error in actual vs desired rate" << endl;
         cerr << "    example -ratescale 0.6:0.5:1.0:1.0" << endl;
-        
+
         cerr << " nsources           : Number of sources, min=1, max=" << MAX_SOURCES << endl;
         cerr << " out_ip             : remote (SCReAM receiver) IP address" << endl;
         cerr << " out_port           : remote (SCReAM receiver) port" << endl;
@@ -362,7 +362,7 @@ int main(int argc, char* argv[]) {
 
     /* Create Receive RTP thread(s) */
     pthread_create(&rx_rtp_thread[0], NULL, rxRtpThread0, "RTP thread 0...");
-    
+
     if (nSources > 1)
         pthread_create(&rx_rtp_thread[1], NULL, rxRtpThread1, "RTP thread 1...");
     if (nSources > 2)
@@ -370,7 +370,7 @@ int main(int argc, char* argv[]) {
     if (nSources > 2)
         pthread_create(&rx_rtp_thread[3], NULL, rxRtpThread3, "RTP thread 3...");
     cerr << "RX RTP thread(s) started" << endl;
-    
+
     /* Create RTCP thread */
     pthread_create(&rx_rtcp_thread, NULL, rxRtcpThread, "RTCP thread...");
     cerr << "RTCP thread started" << endl;
@@ -385,11 +385,11 @@ int main(int argc, char* argv[]) {
         char s[1000];
         time_ntp = getTimeInNtp();
 
-        
+
         if (time_ntp - lastLogT_ntp > 16384) { // 0.25s in Q16
 			lastLogT_ntp = time_ntp;
             char s1[1000];
-            
+
             //pthread_mutex_lock(&lock_scream);
             time_ntp = getTimeInNtp();
             time_s = (time_ntp) / 65536.0f;
@@ -421,8 +421,8 @@ int main(int argc, char* argv[]) {
                 memcpy(s1 + 2 + n * 4, &in_ssrc_network[n], 4);
             }
             sendPacket(s1, 2 + nSources * 4);
-            
-            
+
+
            // pthread_mutex_lock(&lock_scream);
             time_ntp = getTimeInNtp();
             time_s = (time_ntp) / 65536.0f;
@@ -432,28 +432,28 @@ int main(int argc, char* argv[]) {
             if (time_ntp-lastRtcpT_ntp > 8192) {
                 sendBreakSignal = true;
             }
-            
+
             if (sendBreakSignal) {
                 cerr << "RTCP feedback missing" << endl;
                 /*
                 * Signal to RC car to break
-                */            
- 
+                */
+
                 qualityIndex=-1;
             }
             sprintf(s,"%d",qualityIndex);
-            sendto(fd_python,(const char*)s,strlen(s),MSG_CONFIRM,(const struct sockaddr*)&python_addr, sizeof(python_addr));			
-            
-            //pthread_mutex_lock(&lock_scream);            
+            sendto(fd_python,(const char*)s,strlen(s),MSG_CONFIRM,(const struct sockaddr*)&python_addr, sizeof(python_addr));
+
+            //pthread_mutex_lock(&lock_scream);
             time_ntp = getTimeInNtp();
             time_s = (time_ntp) / 65536.0f;
             qualityIndex = int16_t(screamTx->getQualityIndex(time_s,rateMax[0]*1000.0f,0.05f)+0.5f);
             //pthread_mutex_unlock(&lock_scream);
-            
+
             qualityIndex = htons(qualityIndex);
-                        
+
             sendto(fd_out_ctrl, &qualityIndex, 2, 0, (struct sockaddr *)&out_ctrl_addr, sizeof(out_ctrl_addr));
-            
+
             if (fp_log) {
                 fflush(fp_log);
             }
@@ -669,7 +669,7 @@ void *rxRtpThread0(void *arg) {
     }
     /*
      * ToDo delete rtpBufs ?
-     */ 
+     */
     return NULL;
 }
 void *rxRtpThread1(void *arg) {
@@ -735,7 +735,7 @@ void *rxRtcpThread(void *arg) {
 			sprintf(s, "%1.4f", getTimeInNtp() / 65536.0f);
 			screamTx->setTimeString(s);
             pthread_mutex_lock(&lock_scream);
-            screamTx->incomingStandardizedFeedback(getTimeInNtp(), buf_rtcp, recvlen);            
+            screamTx->incomingStandardizedFeedback(getTimeInNtp(), buf_rtcp, recvlen);
             lastRtcpT_ntp = getTimeInNtp();
             pthread_mutex_unlock(&lock_scream);
         } else {
@@ -779,7 +779,7 @@ int setup() {
           return 0;
         }
     }
-    
+
     out_ctrl_addr.sin_family = AF_INET;
     inet_aton(in_ip, (in_addr*)&out_ctrl_addr.sin_addr.s_addr);
     out_ctrl_addr.sin_port = htons(33000);
