dbacb54fc73d5dd96ce567f368f1e04503bb4622
Jacob Teplitsky <jacob.teplitsky@ericsson.com>
2023-06-24 21:27:11 -0700

    fixing Running SCReAM BW test applications in gstscream not work on Linux #51

--- gstscream/scripts/sender_bw.sh
+++ gstscream/scripts/sender_bw.sh
@@ -1,7 +1,14 @@
-export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:../target/debug/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../code/wrapper_lib
+#!/bin/bash
+SCRIPT_PATH=$(realpath  $0)
+SCRIPT_DIR=$(dirname  $SCRIPT_PATH)
+source $SCRIPT_DIR/ecn_env.sh
+SCREAMLIB_DIR=$SCRIPT_DIR/../../code/wrapper_lib
+SCREAM_TARGET_DIR=$SCRIPT_DIR/../target/debug/
+export GST_PLUGIN_PATH=$SCREAM_TARGET_DIR:$GST_PLUGIN_PATH
+export LD_LIBRARY_PATH=$SCREAMLIB_DIR:$LD_LIBRARY_PATH
 
-export SENDPIPELINE=" screamtxbw  name=video params=\"-numframes 10000 -initrate 1000 -ssrc 1\" !  queue ! tee name=t t. ! queue ! screamtx name=screamtx params=\" -forceidr -ect 1 -initrate 2500 -minrate 500 -maxrate 10000 \" !  udpsink host=127.0.0.1 port=30112 sync=false t. ! queue ! fakesink silent=false sync=false rtpbin name=r udpsrc port=30112 address=127.0.0.2 ! queue ! screamtx.rtcp_sink screamtx.rtcp_src ! r.recv_rtcp_sink_0 "
+export SENDPIPELINE=" screamtxbw  name=encoder0 params=\"-numframes 10000 -initrate 1000 -ssrc 1\" !  queue ! tee name=t t. ! queue ! screamtx name=screamtx0 params=\" -forceidr -ect 1 -initrate 2500 -minrate 500 -maxrate 10000 \" !  udpsink host=127.0.0.1 port=30112 sync=false t. ! queue ! fakesink silent=false sync=false rtpbin name=r udpsrc port=30112 address=127.0.0.2 ! queue ! screamtx0.rtcp_sink screamtx0.rtcp_src ! r.recv_rtcp_sink_0 "
 
 killall -9 scream_sender
-../target/debug/scream_sender
+export RUST_BACKTRACE=1
+$SCREAM_TARGET_DIR/scream_sender
